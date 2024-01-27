38b1ea33613b474b8284393bdf33b1fdcfd6cffa
Crowdbotics <team@crowdbotics.com>
2024-01-27 17:42:57 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "icy-band-45642",
+  url: "https://icy-band-45642.botics.co",
+  api: "https://icy-band-45642.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
